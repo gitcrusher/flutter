2e3fba3ff4889c0d041ee91376d7463914fc3179
gitcrusher <aayushguptasoni@gmail.com>
2024-07-25 21:50:11 +0530

    commmit

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Center(
+        child: Text("Aayush"),
+      ),
+    );
+  }
+}
