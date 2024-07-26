5d9b537a2a40c3c736a930ba4eead343c0f28d5d
gitcrusher <aayushguptasoni@gmail.com>
2024-07-26 12:11:02 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 103, 137, 1),
-          title: Center(child: Text('hello World')),
+          backgroundColor: const Color.fromARGB(255, 103, 137, 1),
+          title: const Center(child: Text('hello World')),
         ),
-        backgroundColor: Color.fromARGB(255, 86, 254, 1),
+        backgroundColor: const Color.fromARGB(255, 86, 254, 1),
         body: Center(
           child: OutlinedButton(
               onPressed: () {},
-              child: Text(
+              child: const Text(
                 "plz mujhko daba do",
                 style: TextStyle(
                   color: Color.fromARGB(255, 0, 0, 0),
