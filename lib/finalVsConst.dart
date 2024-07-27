7c4621fa97871610123860476f8ba5bb825e82de
gitcrusher <aayushguptasoni@gmail.com>
2024-07-27 21:15:17 +0530

    commit

---
+++ lib/finalVsConst.dart
@@ -0,0 +1,5 @@
+void main() {
+  print('welcome to dart');
+
+  final name = 'raman';
+}
--- lib/tempCodeRunnerFile.dart
+++
@@ -1,7 +0,0 @@
-var map_name = {
-    'key1': 'value1',
-    'key2': 2,
-    'key3': 3.0,
-    'key4': true,
-  };
-  map_name['key1'] = 'Raman';
